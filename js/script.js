26106a1319fb8ee4fd40de5d4c370c3272b00198
root <root@rexdamorte>
2017-08-06 20:01:33 +0500

    add .

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-/**
- * Created by root on 06.08.17.
- */
+$(document).ready(function () {
+
+    //TABS
+    $(".tab_item").not(":first").hide();
+    $(".wrapper .tab").click(function() {
+        $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $(".tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+    //____________________________________________________________________-
+
+
+    $("body").css("background-color", "blue")
+});
